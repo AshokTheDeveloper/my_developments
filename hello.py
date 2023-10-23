6b2c5b20afba47981370f707f3a6d0db1b165b81
developerashok <ashokchodipalli1@gamil.com>
2023-10-23 12:20:12 +0530

    added area of rectange python code to hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-print("hello world")
+# write a program to find area of rectangle
+
+length_of_rectangle = int(input())
+breadth_of_rectangle = int(input())
+
+area_of_rectange = 2 * ( length_of_rectangle + breadth_of_rectangle)
+print(area_of_rectangle)
